b1631c64070d9bda953f06c84d54dbe77381d5c4
Mperezgil <128558742+Mperezgil@users.noreply.github.com>
2023-05-25 16:05:27 -0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,36 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who created Spongebob?",
+    answer: "Stephen Hillenburg",
+    options: [
+      "Patchy the Pirate",
+      "Stephen Hillenburg",
+      "Tom Kenny",
+      "Mike Gallagher"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How many months has Spongebob had an employee of the month ",
+    answer: "374",
+    options: [
+      "120",
+      "260",
+      "170",
+      "374"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What US state does Sandy come from?",
+    answer: "Texas",
+    options: [
+      "New York",
+      "Ohio",
+      "Texas",
+      "Hawaii"
+    ]
+  }
+];
